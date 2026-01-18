065354dde72231a590c24b6f411a857689fb4db6
kvbson <105971686+kvbson@users.noreply.github.com>
2026-01-18 12:53:59 +0100

    fix

--- api/server/routes/users/loginUser.ts
+++ api/server/routes/users/loginUser.ts
@@ -1,18 +1,20 @@
-import { Router } from 'express';
-import bcrypt from 'bcrypt';
-import { getUserByLogin } from './getUser.js';
+import { Router } from "express";
+import bcrypt from "bcrypt";
+import { getUserByLogin } from "./getUser.js";
 
 const loginUser = Router();
 
-loginUser.post('/', async (req, res) => {
+loginUser.post("/", async (req, res) => {
+  console.log("Login attempt:", req.body, "aaaaaaaaaaaaaaaaaaaaaaaaa");
   const { login, password } = req.body;
-  if (!login || !password) return res.status(400).json({ error: 'Missing password or login' });
+  if (!login || !password)
+    return res.status(400).json({ error: "Missing password or login" });
 
-  const user = await getUserByLogin(login) as Record<string, any> | null;
-  if (!user) return res.status(401).json({ error: 'User not found in DB' });
+  const user = (await getUserByLogin(login)) as Record<string, any> | null;
+  if (!user) return res.status(401).json({ error: "User not found in DB" });
 
   const match = await bcrypt.compare(password, user.password_hash);
-  if (!match) return res.status(401).json({ error: 'Invalid credentials' });
+  if (!match) return res.status(401).json({ error: "Invalid credentials" });
 
   const sess = req.session as any;
   sess.userId = user.id;
