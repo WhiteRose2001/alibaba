426f9849d2068646b677e8978aa35834168f01fd
kvbson <105971686+kvbson@users.noreply.github.com>
2026-01-21 21:51:01 +0100

    .

--- api/server/utils/storageDir.ts
+++ api/server/utils/storageDir.ts
@@ -1,14 +1,13 @@
-import fs from "node:fs";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const storageDir = path
-  .join(__dirname, "../storage/files")
-  .replaceAll("\\", "/");
-console.log("Storage Directory:", storageDir);
+  .join(__dirname, '../storage/files')
+  .replaceAll('\\', '/');
 if (!fs.existsSync(storageDir)) {
   fs.mkdirSync(storageDir, { recursive: true });
 }
