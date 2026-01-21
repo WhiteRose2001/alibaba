4a0e123da8c03b998bb3a9bc8eed2a32fd9b57ae
kvbson <105971686+kvbson@users.noreply.github.com>
2026-01-21 21:47:50 +0100

    fix

--- src/api/clients/callServer.ts
+++ src/api/clients/callServer.ts
@@ -4,7 +4,7 @@ const PATHS: Record<CallServerParams['mode'], string> = {
   UPLOAD: '/files/upload',
   LIST_FILES: '/files/list',
   DELETE_FILE: '/files/delete',
-  DELETE_METADATA: '/files/delete_metadata',
+  DELETE_METADATA: '/files/remove-metadata',
   ADD_USER: '/users/add',
   LOGIN_USER: '/users/login',
   GET_USER: '/users/get',
